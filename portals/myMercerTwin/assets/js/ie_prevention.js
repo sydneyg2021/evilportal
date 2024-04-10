0284b4c9bcdbdf5ee32ffe17647a5628f2010803
Sydney <54872040+sydneyg2021@users.noreply.github.com>
2024-04-10 13:20:04 -0400

    Create ie_prevention.js

---
+++ portals/myMercerTwin/assets/js/ie_prevention.js
@@ -0,0 +1,9 @@
+
+    function showmodal() {
+        $('#divIE').modal('show');
+         document.getElementById('divIE').style.visibility = "visible";
+     }
+     function CloseWindow() {
+        window.open('', '_self', '');
+         window.close();
+     }
