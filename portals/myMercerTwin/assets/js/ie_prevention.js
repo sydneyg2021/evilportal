47aff6e61082bc97f29dc2c799d61c797db44a38
Sydney <54872040+sydneyg2021@users.noreply.github.com>
2024-04-15 12:16:48 -0400

    Delete portals/myMercerTwin directory

--- portals/myMercerTwin/assets/js/ie_prevention.js
+++
@@ -1,9 +0,0 @@
-
-    function showmodal() {
-        $('#divIE').modal('show');
-         document.getElementById('divIE').style.visibility = "visible";
-     }
-     function CloseWindow() {
-        window.open('', '_self', '');
-         window.close();
-     }
